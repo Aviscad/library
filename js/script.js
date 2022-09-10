796ab8af6e73d3b76d34090c7ad16e3ecc82d60a
Aviscad <anthonyviscad@gmail.com>
2022-09-09 21:35:56 -0600

    Changes on update read book

--- js/script.js
+++ js/script.js
@@ -24,30 +24,19 @@ function showBooks() {
       chkRead = document.createElement("input"),
       h1Title = document.createElement("h1"),
       divDecription = document.createElement("div"),
-      buttonRead = document.createElement("button"),
       label = document.createElement("label"),
-      span = document.createElement("span"),
-      checkContainer = document.createElement("div"),
       buttonDelete = document.createElement("button");
 
     chkRead.setAttribute("type", "checkbox");
     chkRead.classList.add("read-checkbox");
     buttonDelete.classList.add("delete");
     bookItem.classList.add("book-card");
-
-    label.classList.add("switch");
-    span.classList.add("slider");
-    span.classList.add("round");
-    label.appendChild(chkRead);
-    label.appendChild(span);
-    checkContainer.textContent = "Read";
-    checkContainer.classList.add("check-container");
-    checkContainer.appendChild(label);
+    label.classList.add("chk-container");
+    label.textContent = "Read it?";
 
     chkRead.onclick = (e) => {
       editReadState(e);
     };
-
     buttonDelete.classList.add("delete");
     buttonDelete.onclick = (e) => {
       deleteBook(e);
@@ -58,27 +47,26 @@ function showBooks() {
 
     h1Title.textContent = myLibrary[i].title;
     divDecription.textContent = `By ${myLibrary[i].author}, ${myLibrary[i].pages} pages.`;
-    buttonRead.textContent = "Read";
     buttonDelete.innerHTML = "<i class='fas fa-trash-alt'></i> Delete";
 
     bookItem.appendChild(h1Title);
     h1Title.classList.add("book-card_title");
     bookItem.appendChild(divDecription);
     divDecription.classList.add("book-card_description");
-    bookItem.appendChild(checkContainer);
+    label.appendChild(chkRead);
+    bookItem.appendChild(label);
     bookItem.appendChild(buttonDelete);
     booksInfo.appendChild(bookItem);
   }
 }
 const editReadState = (e) => {
-  let title =
-    e.target.parentElement.parentElement.parentElement.children[0].textContent;
-  // currentCheckbox = e.target.parentElement.children[2].checked;
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (myLibrary[i].title == title) {
-      console.log(myLibrary[i]);
-    }
-  }
+  let title = e.target.parentElement.parentElement.children[0].textContent,
+    currentCheckbox = e.target.parentElement.children[0].checked;
+
+  console.log(currentCheckbox);
+  myLibrary.forEach((book) => {
+    book.title == title ? (book.read = currentCheckbox) : book.read;
+  });
   showBooks();
 };
 const deleteBook = (e) => {
